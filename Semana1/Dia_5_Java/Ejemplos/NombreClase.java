6898b0203b0287f8b0541ce5d19713bc7103a33e
CeViMu <vce02060@innovaccion.mx>
2022-11-19 20:25:18 -0600

    Agregando ejemplo de strings

---
+++ Semana1/Dia_5_Java/Ejemplos/NombreClase.java
@@ -0,0 +1,10 @@
+package Ejemplos;
+
+public class NombreClase {
+    public static void main(String[] args) {
+        NombreClase referenciaObjeto;
+        referenciaObjeto = new NombreClase();
+        referenciaObjeto.Atributo;
+        referenciaObjeto.metodo([parámetros]);
+    }
+}
