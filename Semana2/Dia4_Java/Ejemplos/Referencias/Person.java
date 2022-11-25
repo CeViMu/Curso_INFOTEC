3f54368a684918adc385f3fdd8ed6ecbc571eb60
CeViMu <vce02060@innovaccion.mx>
2022-11-24 22:21:32 -0600

    Ejercicio de referencias

---
+++ Semana2/Dia4_Java/Ejemplos/Referencias/Person.java
@@ -0,0 +1,11 @@
+package Semana2.Dia4_Java.Ejemplos.Referencias;
+
+public class Person {
+    public Person(String nombre, int edad){
+
+    }
+    public static void main(String[] args) {
+        Person person; //creando una instancia de Person
+        person = new Person("Vianey", 23);//Asiganmos un espacio en memoria
+    }
+}
