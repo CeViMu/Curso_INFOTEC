18582a4ac5c2d594bced7c926080828a2f5086d9
CeViMu <vce02060@innovaccion.mx>
2022-11-26 21:03:15 -0600

    Agregando clase principal

---
+++ Semana2/Dia5_Java/EjercicioPrueba/EjemploJava.java
@@ -0,0 +1,11 @@
+package Semana2.Dia5_Java.EjercicioPrueba;
+
+public class EjemploJava {
+    public static void main(String[] args) {
+        Producto servilleta = new Producto("Servilleta", 140.00);
+        Cliente cliente1 = new Cliente("Juan", 10.00);
+        Tienda tienda = new Tienda("Sonora", "Calle Robles #22", cliente1, servilleta)
+        
+        
+    }
+}
