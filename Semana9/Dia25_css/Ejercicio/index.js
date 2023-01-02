96cc1990abbc2089a962c8614550e9bd8c68e888
CeViMu <vce02060@innovaccion.mx>
2023-01-02 08:56:21 -0600

    Continuando con proyecto simple page

---
+++ Semana9/Dia25_css/Ejercicio/index.js
@@ -0,0 +1,7 @@
+const btnAbrirModal = document.querySelector("#btn-abrir-modal");
+const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+const modal = document.querySelector("#registrate");
+
+btnAbrirModal.addEventListener("click", ()=>{
+    modal.showModal();
+})
